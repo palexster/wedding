b0069c6de8eb304984bb538fed6c144167023417
jakevc <jake.vancampen7@gmail.com>
2018-10-05 22:24:49 -0500

    Update map, and organize js

---
+++ static/js/map.js
@@ -0,0 +1,38 @@
+var map_center;
+var path_bounds;
+function myMap() {
+	var wwitt = {lat: 42.281011,lng:  -122.570865};
+	var mapOptions = {
+    		center: new google.maps.LatLng(42.281011, -122.570865),
+    		zoom: 9,
+    		mapTypeId: google.maps.MapTypeId.HYBRID
+	};
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	var marker = new google.maps.Marker({
+		position: wwitt,
+		map: map, 
+		title: 'Willow Witt Ranch',
+	});
+	resize();
+	google.maps.event.addDomListener(window, 'resize',
+	resize);
+	path_bounds = new google.maps.LatLngBounds();
+};
+function resize() {
+  map.setCenter(wwitt);
+  map.fitBounds(path_bounds);
+};
+window.onload = function() {
+	var div_width = document.getElementById('map').offsetWidth;
+	var div_height = document.getElementById('map').offsetHeight;
+	var set_width = div_width/2;
+	var set_height = div_height/2;
+	document.getElementById('map').style.height = div_height+'px';
+	document.getElementById('map').style.width = div_width+'px';
+	document.getElementById('map').style.position = 'absolute';
+	// document.getElementById('map').style.top = '50%';
+	document.getElementById('map').style.left = '50%';
+	document.getElementById('map').style.marginLeft = -set_width+'px';
+	//document.getElementById('map').style.marginTop = -set_height+'px';
+    };
+    
