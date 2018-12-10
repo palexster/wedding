2304ff54dedd481f7eec5789a4f4ce1809e89541
jakevc <jake.vancampen7@gmail.com>
2018-12-09 20:46:37 -0600

    edit map

--- static/js/map.js
+++ static/js/map.js
@@ -11,17 +11,58 @@ function myMap() {
 	var marker = new google.maps.Marker({
 		position: wwitt,
 		map: map, 
-		title: 'Willow Witt Ranch',
-		label: { color: '#00aaff', fontWeight: 'bold', fontSize: '14px', text: 'Willow Witt Ranch' }
+		label: {
+			color: '#00aaff',
+			fontWeight: 'bold',
+			fontSize: '14px',
+			text: 'Willow Witt Ranch'
+			}
 	});
 	resize();
 	google.maps.event.addDomListener(window, 'resize',
 	resize);
 	path_bounds = new google.maps.LatLngBounds();
+	var directionsService = new google.maps.DirectionsService();
+	var directionsDisplay = new google.maps.DirectionsRenderer({
+		draggable: false,
+		map: map,
+		panel: document.getElementById('right-panel')
+	});	
+
+	directionsDisplay.addListener('directions_changed', function () {
+		computeTotalDistance(directionsDisplay.getDirections());
+	});
+
+	displayRoute('Ashland, OR', wwitt, directionsService,
+		directionsDisplay);
 };
 function resize() {
  map.setCenter(wwitt);
  map.fitBounds(path_bounds);
+};
+function displayRoute(origin, destination, service, display) {
+	service.route({
+		origin: origin,
+		destination: destination,
+		waypoints: [{ location: 'Ashland, OR' }, { location: wwitt }],
+		travelMode: 'DRIVING',
+		avoidTolls: true
+	}, function (response, status) {
+		if (status === 'OK') {
+			display.setDirections(response);
+		} else {
+			alert('Could not display directions due to: ' + status);
+		}
+	});
+};
+function computeTotalDistance(result) {
+	var total = 0;
+	var myroute = result.routes[0];
+	for (var i = 0; i < myroute.legs.length; i++) {
+		total += myroute.legs[i].distance.value;
+	}
+	total = total / 1000;
+	document.getElementById('total').innerHTML = total + ' km';
 };
 window.onload = function() {
 	var div_width = document.getElementById('map').offsetWidth;
