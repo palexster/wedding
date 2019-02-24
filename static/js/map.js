15f7443f61f01492289c27ebe57bc0ac6a65ac1e
Jake VanCampen <jake.vancampen7@gmail.com>
2019-02-24 11:13:36 -0800

    update map

--- static/js/map.js
+++ static/js/map.js
@@ -1,49 +1,57 @@
-var map_center;
-var path_bounds;
-function myMap() {
-	var wwitt = {lat: 42.281011,lng:  -122.570865};
-	var mapOptions = {
-    		center: new google.maps.LatLng(42.281011, -122.570865),
-    		zoom: 9,
-    		mapTypeId: google.maps.MapTypeId.HYBRID
-	};
-	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-	var marker = new google.maps.Marker({
-		position: wwitt,
-		map: map, 
-		label: {
-			color: '#00aaff',
-			fontWeight: 'bold',
-			fontSize: '14px',
-			text: 'Willow Witt Ranch'
-			}
-	});
-	function resize() {
-		map.setCenter(wwitt);
-		map.fitBounds(path_bounds);
-	   };
-	resize();
-	google.maps.event.addDomListener(window, 'resize',
-	resize);
-	path_bounds = new google.maps.LatLngBounds();
-	var directionsService = new google.maps.DirectionsService();
-	var directionsDisplay = new google.maps.DirectionsRenderer({
-		draggable: false,
-		map: map,
-		panel: document.getElementById('right-panel')
-	});	
-};
-window.onload = function() {
-	var div_width = document.getElementById('map').offsetWidth;
-	var div_height = document.getElementById('map').offsetHeight;
-	var set_width = div_width/2;
-	var set_height = div_height/2;ew
-	document.getElementById('map').style.height = div_height+'px';ew
-	document.getElementById('map').style.width = div_width+'px';
-	document.getElementById('map').style.position = 'absolute';
-	document.getElementById('map').style.top = '50%';
-	document.getElementById('map').style.left = '50%';
-	document.getElementById('map').style.marginLeft = -set_width+'px';
-	document.getElementById('map').style.marginBottom = set_height+'px';
-    };
-    
+/*
+ * declare map as a global variable
+ */
+var map;
+
+/*
+ * use google maps api built-in mechanism to attach dom events
+ */
+google.maps.event.addDomListener(window, "load", function () {
+
+  /*
+   * create map
+   */
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: new google.maps.LatLng(33.808678, -117.918921),
+    zoom: 9,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  /*
+   * create infowindow (which will be used by markers)
+   */
+  var infoWindow = new google.maps.InfoWindow();
+
+  /*
+   * marker creater function (acts as a closure for html parameter)
+   */
+  function createMarker(options, html) {
+    var marker = new google.maps.Marker(options);
+    if (html) {
+      google.maps.event.addListener(marker, "click", function () {
+        infoWindow.setContent(html);
+        infoWindow.open(options.map, this);
+      });
+    }
+    return marker;
+  }
+
+  /*
+   * add markers to map
+   */
+  var marker0 = createMarker({
+    position: new google.maps.LatLng(33.808678, -117.918921),
+    map: map,
+    icon: "http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png"
+  }, "<h1>Marker 0</h1><p>This is the home marker.</p>");
+
+  var marker1 = createMarker({
+    position: new google.maps.LatLng(33.818038, -117.928492),
+    map: map
+  }, "<h1>Marker 1</h1><p>This is marker 1</p>");
+
+  var marker2 = createMarker({
+    position: new google.maps.LatLng(33.803333, -117.915278),
+    map: map
+  }, "<h1>Marker 2</h1><p>This is marker 2</p>");
+});
